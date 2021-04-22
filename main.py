6f79e9e3a302a15297862f8a7cdd9076e0305a11
n1k0din <nik726@gmail.com>
2021-04-22 16:09:37 +0700

    Download book covers

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import os.path
 import requests
 import urllib3
-import urllib.parse
 from pathlib import Path
+from urllib.parse import urljoin, unquote, urlsplit
 
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 BOOKS_DIR = 'books/'
+IMAGES_DIR = 'img/'
+
+
+def get_filename(file_url):
+    unquoted_url = unquote(file_url)
+    _, _, path, _, _ = urlsplit(unquoted_url)
+    _, filename = os.path.split(path)
+    # name, ext = os.path.splitext(filename)
+
+    return filename
 
 
 def download_tululu_book(id):
-    url = get_tululu_book_text_url(id)
 
-    author_title = get_author_title_tululu_book(id)
+    author_title_img = get_author_title_img_tululu_book(id)
 
-    if author_title:
-        author, title = author_title
-        filename = f'{id}. {title}.txt'
-        download_txt(url, filename)
+    if author_title_img:
+        author, title, img_url = author_title_img
+
+        text_url = get_tululu_book_text_url(id)
+        text_filename = f'{id}. {title}.txt'
+        download_txt(text_url, text_filename)
+
+        download_img(img_url, get_filename(img_url))
 
 
 def get_tululu_book_text_url(id):
     url = 'https://tululu.org/'
     download_path = f'txt.php?id={id}'
-    return urllib.parse.urljoin(url, download_path)
+    return urljoin(url, download_path)
 
 
 def check_for_redirect(response):
@@ -32,7 +45,7 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def get_author_title_tululu_book(id):
+def get_author_title_img_tululu_book(id):
     url = f'https://tululu.org/b{id}/'
 
     response = requests.get(url, verify=False)
@@ -46,11 +59,22 @@ def get_author_title_tululu_book(id):
 
     soup = BeautifulSoup(response.text, 'lxml')
 
-    author_title = soup.find('div', id='content').find('h1').text
+    try:
+        author_title = soup.find('div', id='content').find('h1').text
+    except AttributeError:
+        print("Can't find title")
+        return None
+
+    try:
+        img_src = soup.find('div', class_='bookimage').find('img')['src']
+    except AttributeError:
+        print("Can't find img")
+        return None
 
     title, author = [name.strip() for name in author_title.split('::')]
+    img_url = urljoin(url, img_src)
 
-    return author, title
+    return author, title, img_url
 
 
 def download_txt(url, filename, folder='books/'):
@@ -80,9 +104,37 @@ def download_txt(url, filename, folder='books/'):
     return filepath
 
 
+def download_img(url, filename, folder='img/'):
+    """Функция для скачивания изображений.
+    Args:
+        url (str): Cсылка на изображение, которое хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранено изображение.
+    """
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    try:
+        check_for_redirect(response)
+    except requests.HTTPError:
+        print(f'Img {filename} not found')
+        return
+
+    sanitized_filename = sanitize_filename(filename)
+    filepath = os.path.join(folder, sanitized_filename)
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+    return filepath
+
+
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     Path(BOOKS_DIR).mkdir(parents=True, exist_ok=True)
+    Path(IMAGES_DIR).mkdir(parents=True, exist_ok=True)
 
     for id in range(1, 10 + 1):
         download_tululu_book(id)
