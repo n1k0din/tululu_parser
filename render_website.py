9bb31fe6fe69157707013b736b11efe9a16d7a47
n1k0din <nik726@gmail.com>
2021-05-20 16:45:45 +0700

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,7 +1,10 @@
 import json
 import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from functools import partial
+
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 
 def fix_path_sep_to_posix(books_metadata: list[dict[str, str]]):
@@ -11,12 +14,7 @@ def fix_path_sep_to_posix(books_metadata: list[dict[str, str]]):
             book['img_path'] = book['img_path'].replace('\\', '/')
 
 
-def main():
-    with open('sci_fi_books.json') as f:
-        books_metadata = json.load(f)
-
-    fix_path_sep_to_posix(books_metadata)
-
+def build_index(books):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -25,14 +23,25 @@ def main():
     template = env.get_template('template.html')
 
     rendered_page = template.render(
-        books=books_metadata,
+        books=books,
     )
 
-    with open('index.html', 'w', encoding="utf8") as file:
+    with open('index.html', 'w', encoding='utf8') as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():
+    with open('sci_fi_books.json') as f:
+        books_metadata = json.load(f)
+
+    fix_path_sep_to_posix(books_metadata)
+
+    rebuild = partial(build_index, books=books_metadata)
+
+    rebuild()
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
