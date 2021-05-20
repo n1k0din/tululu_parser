701de816d98295baa3aea8f36b8ef2ebf183d856
n1k0din <nik726@gmail.com>
2021-05-20 18:02:15 +0700

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -7,6 +7,8 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
+PAGES_DIR = 'pages'
+
 
 def fix_path_sep_to_posix(books_metadata: list[dict[str, str]]):
     if os.sep == '\\':
@@ -15,7 +17,7 @@ def fix_path_sep_to_posix(books_metadata: list[dict[str, str]]):
             book['img_path'] = book['img_path'].replace('\\', '/')
 
 
-def build_index(books):
+def build_index(books, dir='.', set_size=10):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -23,21 +25,27 @@ def build_index(books):
 
     template = env.get_template('template.html')
 
-    rendered_page = template.render(
-        books=chunked(books, 2),
-    )
+    filename_temlate = 'index{}.html'
+    for num, books_set in enumerate(chunked(books, set_size), start=1):
 
-    with open('index.html', 'w', encoding='utf8') as file:
-        file.write(rendered_page)
+        rendered_page = template.render(
+            books=chunked(books_set, 2),
+        )
+        filename = filename_temlate.format(num)
+        full_path = os.path.join(dir, filename)
+
+        with open(full_path, 'w', encoding='utf8') as file:
+            file.write(rendered_page)
 
 
 def main():
+    os.makedirs(PAGES_DIR, exist_ok=True)
     with open('sci_fi_books.json') as f:
         books_metadata = json.load(f)
 
     fix_path_sep_to_posix(books_metadata)
 
-    rebuild = partial(build_index, books=books_metadata)
+    rebuild = partial(build_index, books=books_metadata, dir=PAGES_DIR)
 
     rebuild()
     server = Server()
